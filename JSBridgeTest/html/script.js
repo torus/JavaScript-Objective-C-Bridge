edcb4a8a7b0e346a5e4563db31ec9e07649be8a6
Toru Hisai <toru-github@torus.jp>
2011-04-09 15:48:28 -1000

    clean up

--- JSBridgeTest/html/script.js
+++ JSBridgeTest/html/script.js
@@ -145,41 +145,6 @@ JSBridgeStack.prototype.execute = function () {
 
 ///////////////////
 
-function init () {
-    $(document).ready (function () {
-        $("pre").text ("ready")
-
-        var jsb = new JSBridgeStack ()
-        jsb.push (123, 456).operate ("add").push ("hoge", 1).operate ("callback").execute ()
-    })
-}
-
-function hoge (x) {
-    $("pre").append ("\n" + x)
-
-    var jsb = new JSBridgeStack ()
-    jsb.push ("hoge", "key").operate ("hmac_sha1").operate ("base64data").operate ("print").push ("hoge2", 0).operate ("callback").execute ()
-}
-
-function hoge2 () {
-    $("pre").append ("\n" + "hoge2")
-
-    var url = "http://scrw.in/"
-    var jsb = new JSBridgeStack ()
-    jsb.push ("Value1", "X-Scrw-Key1", "Value2", "X-Scrw-Key2", 2, url).operate ("http_post").push ("hoge3", 1).operate ("callback").execute ()
-}
-
-function hoge3 (connid) {
-    $("pre").append ("\n" + "hoge3: conn ID: " + connid)
-
-    var mesg = '<chat-entry room="opakapaka"><from><user-by-nickname><string>Toru</string></user-by-nickname><avatar-image><string>http://www.gravatar.com/avatar/5efc507a8db7167e2db7889a5597a3cd?s=40&amp;default=identicon</string></avatar-image></from><content><string>konichiwa</string></content></chat-entry>'
-
-    var jsb = new JSBridgeStack ()
-    jsb.pushdata (mesg).push ("Value1", "X-Scrw-Key1", "Value2", "X-Scrw-Key2", 2, "http://scrw.in/push.cgi").operate ("http_post").operate ("print").execute ()
-}
-
-// init ();
-
 function escape_utf8 (str) {
     return encodeURIComponent (str)
 }
